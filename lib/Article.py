5f65c5078b5b8a8d5779ccc4bd24a0f0e44089b3
Babuze <134912091+Babuze1@users.noreply.github.com>
2023-08-21 09:42:15 +0300

    Update Article.py

---
+++ lib/Article.py
@@ -0,0 +1,23 @@
+class Article:
+    def __init__(self, author, magazine, title):
+        self._author = author
+        self._magazine = magazine
+        self._title = title
+
+    def title(self):
+        return self._title
+
+    def author(self):
+        return self._author
+
+    def magazine(self):
+        return self._magazine
+
+    @classmethod
+    def all(cls):
+        # Return a list of all Article instances
+        pass
+
+
+from Author import Author
+from Magazine import Magazine
